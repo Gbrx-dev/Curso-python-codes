b7dcc8b36734a2a2235883f856dca2cd6348dd20
Gabriel Santos <gabrielhenriquex1566@gmail.com>
2025-08-15 11:30:27 -0300

    aulas

---
+++ Aulas/167-DecoradoresParametros.py
@@ -0,0 +1,19 @@
+def fabrica_de_funcoes(func):
+    print('Decoradora 1')
+
+    def aninhada(*args, **kwargs):
+        print('Aninhada')
+        res = func(*args, **kwargs)
+        return res
+    return aninhada
+
+@fabrica_de_funcoes
+def soma(x, y):
+    return x + y
+
+multiplica = fabrica_de_funcoes(lambda x,y: x * y)
+
+dez_mais_cinco = soma(10, 5)
+dez_vezes_cinco = multiplica(10, 5)
+print(dez_mais_cinco)
+print(dez_vezes_cinco)
---
+++ Aulas/aula158.py
@@ -0,0 +1,11 @@
+# from sys import path
+
+# from aula158_package.modulo import soma_do_modulo
+# from aula158_package import modulo
+
+# # print(*path, sep= '\n')
+
+# print(soma_do_modulo(2,5))
+# print(modulo.soma_do_modulo(2,5))
+
+
