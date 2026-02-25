af2d34288d8468c940fd7974fa8866cf9db1cb1f
Gbrx Dev <gabrielhenriquex1566@gmail.com>
2026-02-25 09:22:55 -0300

    aulas

---
+++ Aulas/206-Aula.py
@@ -0,0 +1,17 @@
+# Atributos de classe
+
+class Pessoa:
+    ano_atual = 2026
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+    
+p1 = Pessoa('João', 25)
+p2 = Pessoa('Maria', 16)
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
