0138498c12c68d40e351f05081c3a472bd80cbca
Gbrx Dev <gabrielhenriquex1566@gmail.com>
2026-03-12 12:39:01 -0300

    aulas

---
+++ Aulas/aula223.py
@@ -0,0 +1,37 @@
+# super() e a sobreposição de membros - Python Orientado a Objetos
+# Classe principal (Pessoa)
+#   -> super class, base class, parent class
+# Classes filhas (Cliente)
+#   -> sub class, child class, derived class
+
+# class MinhaString(str):
+#     def upper(self):
+#         print('CHAMOU UPPER')
+#         returno = super().upper()
+#         print('DEPOIS DO UPPER')
+#         return returno
+
+# string = MinhaString('Gabriel')
+# print(string.upper())
+
+class A:
+    atributo_a = 'valor a'
+    def metodo(self):
+        print('A')
+
+class B(A):
+    atributo_b = 'valor b'
+    def metodo(self):
+        print('B')
+
+class C(B):
+    atributo_c = 'valor c'
+    def metodo(self):
+        super().metodo()
+        print('C')
+
+c = C()
+print(c.atributo_a)
+print(c.atributo_b)
+print(c.atributo_c)
+c.metodo()
